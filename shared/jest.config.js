aab861155906ca038a0b0bb90d8b5306d9b4c377
Pedro Luiz <p3dr0c4@live.com>
2023-04-12 18:54:29 -0300

    chore: fix path aliases on jest

--- shared/jest.config.js
+++ shared/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     testMatch: ['<rootDir>/src/**/*.spec.ts'],
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1',
+    }
   };
