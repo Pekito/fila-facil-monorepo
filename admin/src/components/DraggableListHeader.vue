743f378f31cbf05cd576602ab17971b122a0c099
Pedro Luiz <p3dr0c4@live.com>
2023-04-12 17:36:37 -0300

    feat: add clear action to draggable list header

--- admin/src/components/DraggableListHeader.vue
+++ admin/src/components/DraggableListHeader.vue
@@ -8,15 +8,42 @@
     </q-input>
     <q-btn-group>
       <q-btn class="draggable-list-header__button draggable-list-header__button--add" icon="mdi-plus" />
-      <q-btn class="draggable-list-header__button draggable-list-header__button--clear" icon="mdi-delete" />
+      <q-btn class="draggable-list-header__button draggable-list-header__button--clear" icon="mdi-delete" @click="handleClearListClick"/>
     </q-btn-group>
     </header>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
-
+import { inject, ref } from 'vue';
+import { useOrderQueueStore } from '@/stores/order-queue';
+import { useQuasar } from 'quasar';
+import {getListLabel} from "@/helpers/string-helpers";
+import { TListTypes } from '@/types';
+const listName = inject("listContext") as TListTypes;
+const listLabel = getListLabel(listName);
 const text = ref("");
+const orderQueueStore = useOrderQueueStore();
+const $q = useQuasar();
+function handleClearListClick() {
+    $q.dialog({
+        message: `Você deseja apagar TODOS itens ${listLabel}?`,
+        cancel: {
+            label: "Cancelar",
+            color: "negative"
+        },
+        ok: {
+            label: "Apagar",
+            color: "primary"
+        }
+      }).onOk(() => {
+            orderQueueStore.clearList(listName);
+            $q.notify({
+                    message: 'Lista limpa com sucesso',
+                    icon: 'mdi-check-bold',
+                })
+            })
+}
+
 </script>
 
 <style scoped lang="scss">
