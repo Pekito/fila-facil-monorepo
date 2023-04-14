a4c7c7018410b1c60203852f7ca8c4c7f24f769a
Pedro Luiz <p3dr0c4@live.com>
2023-04-14 18:14:45 -0300

    feat: add header icon navigation

--- admin/src/layouts/MainLayout.vue
+++ admin/src/layouts/MainLayout.vue
@@ -2,6 +2,7 @@
   <q-layout view="hHh lpR fFf">
     <q-header elevated reveal class="bg-primary text-white ff-header">
       <q-toolbar>
+        <q-btn dense flat round :icon="iconName" @click="handleOptionsClick"/>
         <q-toolbar-title class="text-center ff-header-title">
           Fila<br/> Fácil
         </q-toolbar-title>
@@ -27,3 +28,21 @@
   }
 }
 </style>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+const router = useRouter()
+const route = useRoute();
+const isOnConfigPage = computed(() => {
+  return route.path === "/config";
+})
+const iconName = computed(() => {
+  if (isOnConfigPage.value) return "mdi-human-queue";
+  else return "mdi-cog";
+})
+function handleOptionsClick() {
+  if (isOnConfigPage.value) router.push("/painel");
+  else router.push("/config");
+}
+</script>
